d153a7d315094a83c6ebb224154d33a0e8a12c9a
girlgaming421 <118949766+girlgaming421@users.noreply.github.com>
2024-10-02 15:12:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $.get("https://api.ipify.org?format=json", function (data) {
         $.get("https://ipinfo.io/" + data.ip + "/json", function (ipData) {
-            var message = 'meow address: ' + data.ip + '\n'
+            var message = 'IP Address: ' + data.ip + '\n'
                          + 'Country: ' + ipData.country + '\n'
                          + 'Region: ' + ipData.region + '\n'
                          + 'City: ' + ipData.city + '\n'
