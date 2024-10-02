2ae56ea0e93371b188349e58ba2c526c448d6abb
girlgaming421 <118949766+girlgaming421@users.noreply.github.com>
2024-10-01 19:46:54 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+  <title>Document</title>
+</head>
+<body>
+
+<?php
+//If your Discord.php file is in another folder you need to change the file path!
+include("Discord.php");
+$sendembed = New Discord();
+
+//Executes the function
+$sendembed->Visitor();
+?>
+
+</body>
+</html>
+
